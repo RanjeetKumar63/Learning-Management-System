ae841b8a39c0c9a144ca37bcdf05e6012de2acb3
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2025-02-15 16:50:42 +0530

    LMS

---
+++ src/components/educator/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>Edu_Footer</div>;
+};
+
+export default Footer;
---
+++ src/components/educator/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Edu navbar</div>;
+};
+
+export default Navbar;
