334998cff5051b9f00861c6ae470941d5fcc2d73
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2025-02-26 13:26:08 +0530

    update

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/student/Loading";
 
 const CourseDetails = () => {
-  return <div>CourseDetails</div>;
+  const { id } = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const { allCourses } = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0  w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70"></div>
+        {/* left colm*/}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
+            {courseData.courseTitle}
+          </h1>
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+          {/* /review and ratings */}
+        </div>
+        {/* Right colm */}
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
 };
 
 export default CourseDetails;
